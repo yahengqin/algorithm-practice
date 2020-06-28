c8f191f9b3774581d7e38ccb3940aa76abaf617f
yaheng.qyh <yaheng.qyh@antfin.com>
2020-06-28 21:04:33 +0800

    add test demo

---
+++ src/test/java/org/tomato/algorithmpractice/DemoTest.java
@@ -0,0 +1,20 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2020 All Rights Reserved.
+ */
+package org.tomato.algorithmpractice;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author qinqixiang
+ * @version $Id: DemoTest.java, v 0.1 2020年06月28日 9:03 PM qinqixiang Exp $
+ */
+public class DemoTest {
+
+    @Test
+    public void test() {
+        System.out.println("this is a test demo");
+    }
+}
