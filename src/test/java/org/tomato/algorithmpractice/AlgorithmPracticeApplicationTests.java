62faa6dd393473ec40d7cf058a3d077fe5d8f9ad
yaheng.qyh <yaheng.qyh@antfin.com>
2020-06-28 20:18:28 +0800

    init code

---
+++ src/test/java/org/tomato/algorithmpractice/AlgorithmPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tomato.algorithmpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
