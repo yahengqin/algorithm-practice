e087d155c567b9e0d1fbdd5e514312c33f8a5042
yaheng.qyh <yaheng.qyh@antfin.com>
2020-07-02 11:42:26 +0800

    heap sort

--- src/test/java/org/tomato/algorithmpractice/FindKthLargest.java
+++ src/test/java/org/tomato/algorithmpractice/FindKthLargest.java
@@ -20,9 +20,12 @@ public class FindKthLargest {
 
     @Test
     public void test() {
-        System.out.println(findKthLargest(new int[] {1, 22, 3, 5, 6}, 2));
-        System.out.println(findKthLargest(new int[] {1, 22, 3, 5, 6}, 2));
-        System.out.println(findKthLargestOfFastSort(new int[] {2, 22, 2, 2, 2}, 2));
+        assert findKthLargest(new int[] {1, 22, 3, 5, 6}, 2) == 6;
+        assert findKthLargestOfFastSort(new int[] {2, 22, 2, 2, 2}, 2) == 2;
+        assert findKthLargestOfHeap(new int[] {1, 22, 3, 5, 6}, 2) == 6;
+        assert findKthLargestOfHeap(new int[] {3, 2, 1, 5, 6, 4}, 2) == 5;
+        assert findKthLargestOfHeap(new int[] {2, 22, 2, 2, 2}, 2) == 2;
+
     }
 
     public int findKthLargest(int[] nums, int k) {
@@ -30,13 +33,51 @@ public class FindKthLargest {
         return list.get(list.size() - k);
     }
 
-    // TODO 堆排序
+    // 堆排序
     public int findKthLargestOfHeap(int[] nums, int k) {
+        int heap = nums.length - 1;
+        int result = 0;
 
-        return 0;
+        for (int i = 0; i < k; i++) {
+            nums = buildMaxHeap(nums, heap);
+            result = nums[0];
+            swap(nums, 0, heap);
+            heap--;
+        }
+
+        return result;
     }
 
-    // 所见寻找速度，最好二分一下只处理目标分段
+    private int[] buildMaxHeap(int[] nums, int endLength) {
+        // 完全二叉树计算第一个非叶子节点公式：数组长度/2 -1
+        int key = ((endLength + 1) / 2) - 1;
+
+        while (key >= 0) {
+            improveHeap(nums, key, endLength);
+            key--;
+        }
+
+        return nums;
+    }
+
+    private void improveHeap(int[] nums, int point, int endLength) {
+        int l = 2 * point + 1;
+        int r = 2 * point + 2;
+        int largest = point;
+
+        if (l > 0 && l <= endLength && nums[largest] < nums[l]) {
+            largest = l;
+        }
+        if (r > 0 && r <= endLength && nums[largest] < nums[r]) {
+            largest = r;
+        }
+        if (largest != point) {
+            swap(nums, largest, point);
+            improveHeap(nums, largest, endLength);
+        }
+    }
+
+    // 缩短寻找速度，最好二分一下只处理目标分段
     public int findKthLargestOfFastSort(int[] nums, int k) {
         int key = fastSort(nums, 0, nums.length - 1);
 
@@ -67,9 +108,9 @@ public class FindKthLargest {
         return key;
     }
 
-    private void swap(int[] nums, int left, int right) {
-        int temp = nums[left];
-        nums[left] = nums[right];
-        nums[right] = temp;
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
